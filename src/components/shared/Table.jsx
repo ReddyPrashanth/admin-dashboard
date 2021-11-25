3fb230730f1eb302ca509574c632e1191ca91b2e
ReddyPrashanth <s.prasantreddy94@gmail.com>
2021-11-25 05:38:00 -0600

    added right arrow icon

--- src/components/shared/Table.jsx
+++ src/components/shared/Table.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import RightArrowIcon from '../../icons/RightArrow';
 
 class Table extends React.Component {
     createHeader(th) {
@@ -26,8 +27,10 @@ class Table extends React.Component {
                 if(col.name !== 'address') return <td key={col.name} className="px-6 py-4">{item[col.name]}</td>
                 return <td key={col.name} className="px-6 py-4">{item[col.name].address1}</td>
             })}
-            <td className="px-6 py-4">
-                <button key="view" className="text-purple-600 hover:text-purple-800">view</button>
+            <td className="px-6 py-2">
+                <button key="view" className="text-purple-600 hover:text-purple-800 border bg-gray-200 rounded p-2 hover:bg-gray-300">
+                    <RightArrowIcon />
+                </button>
             </td>
         </tr>
     }
